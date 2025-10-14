d87c96832ee394117f42a43e5f211dcbff212c00
Daria Egarmina <d.egarmina@gmail.com>
2025-10-14 17:37:04 +0300

    Создает компонент для главной страницы, создает компонент App для отрисовки главной страницы
    Создает компоненты для основных страниц

---
+++ src/components/app/app.tsx
@@ -0,0 +1,13 @@
+import MainScreen from '../../pages/main-screen/main-screen';
+
+type AppScreenProps = {
+  placesFound: number;
+};
+
+function App({ placesFound }: AppScreenProps): JSX.Element {
+  return (
+    <MainScreen placesFound={placesFound} />
+  );
+}
+
+export default App;
