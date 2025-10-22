392e1a089258add86061ca19137a4f51025ff537
Daria Egarmina <d.egarmina@gmail.com>
2025-10-22 17:48:38 +0300

    Реализует маршрутизацию WIP

---
+++ src/pages/notFound-screen/notFound-screen.tsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+function NotFoundScreen(): JSX.Element {
+  return (
+    <>
+      <h1>Ошибка 404. Страница не существует.</h1>
+      <Link to='/'>Вернуться на главную страницу</Link>
+    </>
+  );
+}
+
+export default NotFoundScreen;
