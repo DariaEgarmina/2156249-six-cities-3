03d19fb24964c3b3e966f6aebb9c0378d094bb88
Daria Egarmina <d.egarmina@gmail.com>
2026-01-15 13:34:56 +0300

    Добавляет в api.ts interceptor для добавления токена в заголовки запросов

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosInstance } from 'axios';
+import { getToken } from './token';
 
 export const BACKEND_URL = 'https://15.design.htmlacademy.pro/six-cities';
 export const REQUEST_TIMEOUT = 5000;
@@ -7,6 +8,18 @@ export const createAPI = (): AxiosInstance => {
   const api = axios.create({
     baseURL: BACKEND_URL,
     timeout: REQUEST_TIMEOUT,
+  });
+
+  // у меня лезет ошибка, если использую config: AxiosRequestConfig - поэтому пока убрала этот тип
+  //как лучше сделать?
+  api.interceptors.request.use((config) => {
+    const token = getToken();
+
+    if (token && config.headers) {
+      config.headers['x-token'] = token;
+    }
+
+    return config;
   });
 
   return api;
