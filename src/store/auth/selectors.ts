7f0305a5d2ae303b3622aa475e691276fa36e657
Daria Egarmina <d.egarmina@gmail.com>
2026-01-26 17:24:35 +0300

    Добавляет auth слайс в store
    Создаёт index.ts для ре-экспорта auth модуля
    
    Добавляет auth селекторы
    
    Подключает auth редьюсер в корневой reducer

---
+++ src/store/auth/selectors.ts
@@ -0,0 +1,15 @@
+import { State } from '@/store';
+import { NameSpace, AuthorizationStatus } from '@/const';
+
+export const getAuthStatus = (state: State) =>
+  state[NameSpace.Auth].authorizationStatus;
+
+export const getUserData = (state: State) => state[NameSpace.Auth].userData;
+
+export const getIsSubmitting = (state: State) =>
+  state[NameSpace.Auth].isSubmitting;
+
+export const getAuthError = (state: State) => state[NameSpace.Auth].error;
+
+export const isAuth = (state: State) =>
+  state[NameSpace.Auth].authorizationStatus === AuthorizationStatus.Auth;
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,7 +1,9 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { NameSpace } from '@/const';
 import offersReducer from '@/store/offers';
+import authReducer from '@/store/auth';
 
 export const rootReducer = combineReducers({
   [NameSpace.Offers]: offersReducer,
+  [NameSpace.Auth]: authReducer,
 });
