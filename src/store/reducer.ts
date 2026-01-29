157fb92a27b5c6c9a61c4c1b6ece5a19aafb620c
Daria Egarmina <d.egarmina@gmail.com>
2026-01-29 14:14:15 +0300

    Подключает слайс offer к глобальному store
    Добавляет offerReducer в корневой редьюсер
    
    Создает селекторы getOffer, getNearbyOffers, getIsOfferLoading, getOfferError
    
    Создает index.ts для реэкспорта модуля offer

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -2,8 +2,10 @@ import { combineReducers } from '@reduxjs/toolkit';
 import { NameSpace } from '@/const';
 import offersReducer from '@/store/offers';
 import authReducer from '@/store/auth';
+import offerReducer from '@/store/offer';
 
 export const rootReducer = combineReducers({
   [NameSpace.Offers]: offersReducer,
   [NameSpace.Auth]: authReducer,
+  [NameSpace.Offer]: offerReducer,
 });
