5d152c423e67b1917f6e981c07c94a99d47a802f
Daria Egarmina <d.egarmina@gmail.com>
2026-02-26 14:12:47 +0300

    Исправляет инлайн-обработчик в FullPageError

--- src/components/full-page-error/full-page-error.tsx
+++ src/components/full-page-error/full-page-error.tsx
@@ -5,12 +5,16 @@ type FullPageErrorProps = {
 };
 
 function FullPageError({ error }: FullPageErrorProps): JSX.Element {
+  const handleReloadButtonClick = () => {
+    window.location.reload();
+  };
+
   return (
     <div className="full-page-error">
       <h1 className="full-page-error__title">Something went wrong</h1>
       <p className="full-page-error__text">{error || 'Failed to load data'}</p>
       <button
-        onClick={() => window.location.reload()}
+        onClick={handleReloadButtonClick}
         className="full-page-error__button"
         type="button"
       >
