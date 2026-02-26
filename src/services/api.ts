90598b3993966e324cbfd742cf0fd410c77945c6
Daria Egarmina <d.egarmina@gmail.com>
2026-02-26 13:38:20 +0300

    Исправляет название заголовка токена
    Приводит X-Token к точному написанию из ТЗ

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
 import { getToken } from './token';
 
-
 export const BACKEND_URL = 'https://15.design.htmlacademy.pro/six-cities';
 export const REQUEST_TIMEOUT = 5000;
 
@@ -15,7 +14,7 @@ export const createAPI = (): AxiosInstance => {
     const token = getToken();
 
     if (token && config.headers) {
-      config.headers['x-token'] = token;
+      config.headers['X-Token'] = token;
     }
 
     return config;
