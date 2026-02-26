c42aabba8fdbcf8741e8230f9b8bb2751d60595c
Daria Egarmina <d.egarmina@gmail.com>
2026-02-26 14:05:07 +0300

    Удаляет лишние комментарии

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,8 +1,6 @@
-import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { State, AppDispatch } from '@/store';
 
-// Для отправки действий
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 
-// Для чтения данных
 export const useAppSelector: TypedUseSelectorHook<State> = useSelector;
