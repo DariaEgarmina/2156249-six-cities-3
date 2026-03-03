bbe6c9402b6e0b86327b41162338a703828ceaa0
Daria Egarmina <d.egarmina@gmail.com>
2026-03-03 12:48:50 +0300

    Добавляет тесты для всех редьюсеров offersSlice

--- src/store/offers/offers.test.ts
+++ src/store/offers/offers.test.ts
@@ -1,4 +1,10 @@
-import { offersSlice } from './slice';
+import {
+  offersSlice,
+  setCity,
+  setActiveSort,
+  setSelectedOfferId,
+  clearError,
+} from './slice';
 import { CITIES } from '@/const';
 import { OffersState } from './types';
 
@@ -22,5 +28,50 @@ describe('offersSlice', () => {
     const emptyAction = { type: '' };
     const result = offersSlice.reducer(initialState, emptyAction);
     expect(result).toEqual(initialState);
+  });
+
+  describe('reducers', () => {
+    it('should change city with "setCity" action', () => {
+      const newCity = CITIES[1];
+      const action = setCity(newCity);
+      const result = offersSlice.reducer(initialState, action);
+
+      expect(result.city).toBe(newCity);
+      expect(result.offers).toEqual([]);
+    });
+
+    it('should change sort type with "setActiveSort" action', () => {
+      const newSort = 'PriceLowToHigh';
+      const action = setActiveSort(newSort);
+      const result = offersSlice.reducer(initialState, action);
+
+      expect(result.activeSort).toBe(newSort);
+    });
+
+    it('should set offer id with "setSelectedOfferId" action', () => {
+      const offerId = '123';
+      const action = setSelectedOfferId(offerId);
+      const result = offersSlice.reducer(initialState, action);
+
+      expect(result.selectedOfferId).toBe(offerId);
+    });
+
+    it('should set null with "setSelectedOfferId" action', () => {
+      const action = setSelectedOfferId(null);
+      const result = offersSlice.reducer(initialState, action);
+
+      expect(result.selectedOfferId).toBeNull();
+    });
+
+    it('should reset error with "clearError" action', () => {
+      const stateWithError = {
+        ...initialState,
+        error: 'Some error',
+      };
+      const action = clearError();
+      const result = offersSlice.reducer(stateWithError, action);
+
+      expect(result.error).toBeNull();
+    });
   });
 });
